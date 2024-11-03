009ced52504a4903fbd487671d6227174f8b5d91
Mark-Brooks <88011469+Mark-Brooks@users.noreply.github.com>
2024-11-03 16:19:33 +1000

    Update input_handler.py
    changed to use mouse clicks

--- input_handler.py
+++ input_handler.py
@@ -1,22 +1,23 @@
 # input_handler.py
-import RPi.GPIO as GPIO
+import pygame
 
 class InputHandler:
     def __init__(self, spotify_client, display):
         self.spotify = spotify_client
         self.display = display
-        # Rotary encoder GPIO setup (adjust pins as needed)
-        GPIO.setmode(GPIO.BCM)
-        self.encoder_clk = 17
-        self.encoder_dt = 18
-        GPIO.setup(self.encoder_clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.encoder_dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def listen(self):
-        if GPIO.input(self.encoder_clk) == 1:
-            if GPIO.input(self.encoder_dt) == 0:
-                self.spotify.next_track()  # Example for rotary to next track
-                track_info = self.spotify.get_current_track()
-                self.display.update(track_info)
-            else:
-                self.spotify.pause_track()  # Example for button press to pause
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            # Mouse click events to handle button clicks
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.display.play_button.collidepoint(event.pos):
+                    # Toggle play/pause
+                    self.spotify.pause_track() if self.spotify.is_playing() else self.spotify.play_track()
+                elif self.display.next_button.collidepoint(event.pos):
+                    self.spotify.next_track()  # Go to next track
+                elif self.display.prev_button.collidepoint(event.pos):
+                    self.spotify.previous_track()  # Go to previous track
